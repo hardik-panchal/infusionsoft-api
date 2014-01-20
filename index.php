75ef8b9e09d71ebd403240fd472a8435c412f84e
Hardik Panchal <hardikpanchal469@gmail.com>
2014-01-20 17:57:49 +0530

    Order Client

--- index.php
+++ index.php
@@ -6,6 +6,31 @@
  * @version 1.0
  * @package BePure
  * 
+ * credentials:
+ * 
+ *  
+ *  https://signin.infusionsoft.com/login?service=https://os140.infusionsoft.com/j_spring_cas_security_check
+
+    Use: ben@bepure.co.nz
+    Incredible22
+  
+    API KEY: 7b6e7bf7278e41be52eda569b9f563bf
+
+    Then we want this information pushed into Fastway, please use the details below...
+
+    www.fastwaycustomer.com
+
+    Username: info@bepure.co.nz
+    Password: 0630452
+
+    Please use this link to access fastlabel documentation for integration, 
+
+    http://api.fastway.org/v2/docs/index.html?api_key= fc02f7481690e29cc62b6f816fb8cfde 
+
+ * 
+ * 
+ * 
+ * 
  */
 session_start();
 error_reporting(0);
--- instance/front/controller/infusionsoft_order.inc.php
+++ instance/front/controller/infusionsoft_order.inc.php
@@ -1,3 +1,30 @@
 <?php
+
+_errors_on();
+$infusionsoft_api_creds = Config::getData(array("infusionsoft_key", "infusionsoft_app_name", "infusionsoft_domain_name"));
+
+$infusionsoft_domain = $infusionsoft_api_creds['1']['value'];
+$infusinosoft_key = $infusionsoft_api_creds['2']['value'];
+$infusinosoft_app_name = $infusionsoft_api_creds['0']['value'];
+
+require_once(_PATH . "lib/infusionSoft/isdk.php");
+
+$app = new iSDK;
+
+if ($app->cfgCon($infusionsoft_domain, $infusinosoft_key)) {
+
+    echo "connected<br/>";
+    echo "app connected<br/>";
+
+    $data = $app->dsQueryOrderBy();
+    d($data);
+} else {
+    echo "connection failed";
+}
+die;
+
+/**
+ * Infusionsoft order integration page
+ */
 _cg("page_title", "Infusionsoft Orders");
 ?>
