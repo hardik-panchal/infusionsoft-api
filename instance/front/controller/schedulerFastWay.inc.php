2c9c2431de68a446f82926ad3a8513aa475ca480
Hardik Panchal <hardikpanchal469@gmail.com>
2014-01-27 22:22:45 +0530

    Add Consignment

--- instance/front/controller/schedulerFastWay.inc.php
+++ instance/front/controller/schedulerFastWay.inc.php
@@ -20,12 +20,25 @@ if (!$user_id) {
     die("Unable to retrieve UserID");
 }
 
-$manifest = $apiFL->getOpenManifest($user_id);
-$manifest_id = $manifest['result'][0]['ManifestID'];
+//$manifest = $apiFL->getOpenManifest($user_id);
+//$manifest_id = $manifest['result'][0]['ManifestID'];
+//
+//if(!$manifest_id){
+//    die("Unable to get menifeft id");
+//}
+#, Rd 12, Havelock North, 4294, New Zealand
 
-if(!$manifest_id){
-    die("Unable to get menifeft id");
-}
+$data = array();
+
+$data['CompanyName'] = "Company Test";
+$data['Address1'] = "563 Maraetotara Rd";
+$data['Address2'] = "Rd 12";
+$data['Postcode'] = "4294";
+$data['Suburb'] = "Havelock North";
+$data['items'] = array("0" => array("Weight" => "1", "Quantity" => "1", "Packaging" => 1));
+
+$data = $apiFL->createConsignment($user_id, $data);
+d($data);
 
 
 
--- lib/apiFastLabel.class.php
+++ lib/apiFastLabel.class.php
@@ -10,6 +10,7 @@
  * http://api.fastway.org/v2/docs/detail?ControllerName=fastlabel&api_key=fc02f7481690e29cc62b6f816fb8cfde
  * 
  * Looks like simple url retrieval rather than all complex rest/soap client implementation and handshaking
+ * 
  */
 Class apiFastLabel extends apiCore {
 
@@ -36,6 +37,60 @@ Class apiFastLabel extends apiCore {
         $url = $this->prepareApiUrl();
         $mainfest = $this->doCall($url);
         return json_decode($mainfest, true);
+    }
+
+    public function createConsignment($userID, $data) {
+
+        /*
+          http://api.fastway.org/v2/fastlabel/addconsignment?UserID=96&
+         *  CompanyName=Test Company&
+         *  Address1=Test Address 1&
+         *  Suburb=Ahuriri&
+         *  Postcode=4110&
+         *  Items[0].Weight=1&
+         *  Items[0].Quantity=1&
+         *  Items[0].Packaging=1&
+         *  api_key=fc02f7481690e29cc62b6f816fb8cfde
+         * 
+         * */
+
+        $this->apiEndpoint = 'addconsignment';
+        $this->params['UserID'] = $userID;
+        $this->params['CompanyName'] = $data['CompanyName'];
+        $this->params['Address1'] = $data['Address1'];
+        $this->params['Suburb'] = $data['Suburb'];
+        $this->params['Postcode'] = $data['Postcode'];
+        foreach ($data['items'] as $index => $each_item) {
+            if ($each_item['Weight']) {
+                $this->params["Items[{$index}].Weight"] = $each_item['Weight'];
+            }
+            $this->params["Items[{$index}].Quantity"] = $each_item['Quantity'];
+            $this->params["Items[{$index}].Packaging"] = $each_item['Packaging'];
+            
+        }
+
+        $url = $this->prepareApiUrl();
+        $data = $this->doCall($url);
+        return json_decode($data, true);
+
+        #Response
+        # it will have manifest
+        # Cost, consignment id
+        /**
+         * {
+
+          'result': {
+          'CostExGst': '5.25',
+          'CostMarkupExGst': '10',
+          'ConsignmentID': '1289190',
+          'ManifestID': '104408',
+          'LabelNumbers': [
+          'FW0000730421'
+          ]
+          },
+          'generated_in': '76ms'
+          }
+         */
     }
 
 }
